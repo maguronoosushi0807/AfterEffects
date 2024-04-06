e7b33b5c45fa012f89e951b1916eb174a4ff4ea5
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-04-06 20:58:27 +0900

    改行を調べるのに苦戦
    とても無理

--- JsxbinReverser/main.cpp
+++ JsxbinReverser/main.cpp
@@ -1,13 +1,14 @@
 #include<fstream>
 #include<iostream>
+#include<regex>
 #include<string>
 
-namespace Constants {
-	constexpr unsigned int TYPE = 3;
-	constexpr unsigned int CHARACTER_NUM=26;
+namespace {
+	constexpr unsigned int CHARACTER_TYPE = 2;
+	constexpr unsigned int CHARACTER_NUM = 26;
 }
 
-std::string character[Constants::TYPE][Constants::CHARACTER_NUM][2]{
+std::string character[CHARACTER_TYPE][CHARACTER_NUM][2]{
 	{
 		{"jB","a"},
 		{"jC","b"},
@@ -63,154 +64,149 @@ std::string character[Constants::TYPE][Constants::CHARACTER_NUM][2]{
 		{"iY","X"},
 		{"iZ","Y"},
 		{"ia","Z"}
-	},
-	{
-
 	}
 };
 
 /*
-std::string upper[26][2]{
-	{"iB","A"},
-	{"iC","B"},
-	{"iD","C"},
-	{"iE","D"},
-	{"iF","E"},
-	{"iG","F"},
-	{"iH","G"},
-	{"iI","H"},
-	{"iJ","I"},
-	{"iK","J"},
-	{"iL","K"},
-	{"iM","L"},
-	{"iN","M"},
-	{"iO","N"},
-	{"iP","O"},
-	{"iQ","P"},
-	{"iR","Q"},
-	{"iS","R"},
-	{"iT","S"},
-	{"iU","T"},
-	{"iV","U"},
-	{"iW","V"},
-	{"iX","W"},
-	{"iY","X"},
-	{"iZ","Y"},
-	{"iz","Z"},
-};
+
+ 全部取得
+  ↓
+ 整形する
+  ↓
+ 処理する
+
 */
 
+// .jsxbin bin
+// .jsx    script
 
 int main() {
-	std::ifstream file("ExpressionPaster.jsxbin");
-	std::string line;
+	std::ifstream file("ExpressionPaster.jsx");
+	std::string bin = "";
+	std::string script = "";
 
+	//ファイルの読み込み
 	if (!file) {
 		std::cout << "ファイルを読み込めませんでした。" << std::endl;
+		return -1;
 	}
 	else {
-		unsigned int lineCount = 1;
-		//bool isSearching = true;
-		bool hasEnteredOneCharacter = false;
-		std::string kaigyouBuff = "";
+		std::string line;
+		while (std::getline(file, line)) {
+			bin += line;
+		}
 
-		while (true) {
-			//行が空
-			if (!std::getline(file, line))	break;
+		std::cout << "コンパイルを開始します。" << std::endl;
+		//std::cout << bin;
+	}
+	/*
+	else {
+		std::string line;
+		while (std::getline(file, line)) {
+			bin += line;
+		}
 
-			//1行目の場合、前の不要な部分を削除
-			if (lineCount == 1) {
+		//先頭部分を削除
+		std::string header = "@JSXBIN@ES@2.0@MyB";
 
-				//std::getline(file, line);
+		int header_position = bin.find(header);
+		if (header_position == std::string::npos) {
+			std::cout << "予測された先頭要素が見つかりませんでした。" << std::endl;
+		}
+		else {
+			bin.erase(header_position, header.length() + 14);
+		}
 
-				int headerEnd = line.find("MyB");
-				line.erase(0, headerEnd + 18);
-			}
-			//終端を探し、削除
-			int eof = line.find("ByB");
-			if (eof != std::string::npos) {
-				line.erase(eof - 1, 10);
-			}
+		//後端要素を削除
+		std::string footer = "ByB";
+		int footer_position = bin.find(footer);
+		if (footer_position == std::string::npos) {
+			std::cout << "予測された後端要素が見つかりませんでした。" << std::endl;
+		}
+		else {
+			bin.erase(footer_position - 7, 7 + footer.length());
+		}
 
-			//行が空になるまで調べる
-			while (!line.empty()) {
-				//残り1文字で改行された
-				if (!kaigyouBuff.empty()) {
-					line = kaigyouBuff + line;
-					kaigyouBuff.erase(0, 1);
-					//std::cout << "残り1文字で改行されました" << line << std::endl;
-				}
+		//std::cout << bin;
+		std::cout << "逆コンパイルを開始します。" << std::endl << std::endl << std::endl;
+	}
 
 
-				//文字列の残りが1文字
-				if (line.size() == 1) {
-					kaigyouBuff = line.substr(0, 1);
-					//std::cout << "のこり1文字 : " << kaigyouBuff << std::endl;
-					line.erase(0, 2);
-				}
-
-				//2文字取得
-				std::string replaceBuff = line.substr(0, 2);
-				line.erase(0, 1);
-
-				//一致する文字列がないか調べる
-				for (int j = 0; j < Constants::TYPE; j++) {
-					unsigned int searchedCount = 0;
-					for (int i = 0; i < Constants::CHARACTER_NUM; i++) {
-						if (replaceBuff == character[j][i][0]) {
-
-							replaceBuff = character[j][i][1];
-							std::cout << replaceBuff;
-						}
-						else {
-							searchedCount++;
-						}
-					}
-
-					//if (searchedCount == 26) std::cout << "一致する文字列が見つかりませんでした。" << std::endl;
-				}
-			}
-			//std::cout << std::endl;
-
-			lineCount++;
+	for (int j = 0; j < 4; j++) {
+		for (int i = 0; i < CHARACTER_NUM; i++) {
+			std::cout << character[0][i][1] << std::endl;
 		}
 	}
 
+
+	//改行を置き換える
+	std::regex line_break{ R"(.*fJ.*nAjzB)" };	//改行の正規表現（いかつい）
+	std::string buf = bin;
+	bin = std::regex_replace(buf, line_break, "\n");
+	//std::cout << bin;
+
+	//変換処理, 読み込んだバイナリ（？）が空になるまで調べる
+	while (!bin.empty()) {
+	}
+	//2文字取得
+	/*
+	std::string replaceBuff = bin.substr(0, 2);
+	bin.erase(0, 1);
+
+	//一致する文字列がないか調べる
+	for (int j = 0; j < CHARACTER_TYPE; j++) {
+		for (int i = 0; i < CHARACTER_NUM; i++) {
+			int replacePos = 0;
+			while (bin.find(character[j][i][0]) != std::string::npos) {
+				replacePos = bin.find(character[j][i][0]);
+				bin.replace(replacePos, 2, character[j][i][1]);
+			}
+
+
+		}
+	}
+	//break;
+
+	std::cout << bin;
+	*/
+
+	uint32_t lineCount = 0;
+	script += std::to_string(lineCount) + " ";
+
+	//Compile
+	while (!bin.empty()) {
+
+		//改行
+		if (bin.find("\n")==0) {
+			script += "\n";
+			script += std::to_string(++lineCount) + " ";
+		}
+
+		//script += ++lineCount;
+		for (int j = 0; j < CHARACTER_TYPE; j++) {
+			for (int i = 0; i < CHARACTER_NUM; i++) {
+				if (bin.substr(0, 1) == character[j][i][1]) {
+					script += character[j][i][0];
+				}
+			}
+		}
+
+		bin.erase(0, 1);
+	}
+
+	std::cout << script;
 
 	std::cout << std::endl << std::endl << "----------------" << std::endl << std::endl << std::endl;
 	return 0;
 }
 
 /*
-int *a=NULL;
-*a = 10;
-*/
 
 
-/*
-			//行を変換
-			while (!line.empty()) {
+	メモリぶっ壊しゾーン
 
-				std::string buff;
-				lineBuff = line;
-				std::cout << "残り1文字です" << std::endl;
+	int *a=NULL;
+	*a = 10;
 
-				//残り1文字の場合
 
-				buff = line.substr(0, 2);
-				line.erase(0, 2);
-				for (int i = 0; i < 26; i++) {
-					if (buff == lower[i][0]) {
-						buff = lower[i][1];
-					}
-				}
-
-				std::cout << buff;
-			}
-
-			std::cout << std::endl;
-
-			if (!std::getline(file, line)) {
-				isSearching = false;
-			}
 */
